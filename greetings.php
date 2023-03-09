0e1c28343a69e3109c133e5f2b3f81a3f787527f
RiccardoPietrobon <riccardopietrobon.sanso@gmail.com>
2023-03-09 20:06:07 +0100

    ok censured

--- greetings.php
+++ greetings.php
@@ -8,10 +8,11 @@ Il secondo file riceverà la richiesta ed eseguirà queste operazioni:
 - stampare di nuovo il paragrafo e la sua lunghezza, dopo aver sostituito con tre asterischi (***) tutte le occorrenze della parola da censurare -->
 
 <?php
+    /* associo una variabile al valore dato dall input */
     $mio_paragrafo = $_GET["paragrafo"];
     $mia_parola = $_GET["parola"];
-    
-
+    /* sostituisco con la funzione il valore della stringa indesiderata */
+    $paragrafo_censured = str_replace($mia_parola, "***", $mio_paragrafo);
 ?>
 
 <!DOCTYPE html>
@@ -23,8 +24,11 @@ Il secondo file riceverà la richiesta ed eseguirà queste operazioni:
     <title>Dati</title>
 </head>
 <body>
+    <h1>Dati ricevuti</h1>
+    <!-- con echo stampo i valori -->
     <p>Il testo è: "<?php echo $mio_paragrafo;?>"</p>
     <p>La lunghezza del testo è: "<?php echo strlen($mio_paragrafo);?>"</p>
+    <p>Il testo censurato è: "<?php echo $paragrafo_censured;?>"</p>
 
 </body>
 </html>
